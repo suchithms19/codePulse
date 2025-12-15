dfbaf8c2df3838ee751aac9804cbb4711797bf4a
suchithms19 <mssuchith2@gmail.com>
2025-12-15 09:07:43 +0530

    Add initial categoriesController API controller

---
+++ codePulse.API/Controllers/categoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace codePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class categoriesController : ControllerBase
+    {
+    }
+}
