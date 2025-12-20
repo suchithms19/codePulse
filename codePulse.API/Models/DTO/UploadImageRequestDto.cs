8c611fbcd795b49391477db4460078012f855a20
suchithms19 <mssuchith2@gmail.com>
2025-12-21 01:49:23 +0530

    feat: add required DTOs for images

---
+++ codePulse.API/Models/DTO/UploadImageRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace codePulse.API.Models.DTO
+{
+    public class UploadImageRequestDto
+    {
+        public IFormFile File { get; set; }
+        public string FileName { get; set; }
+        public string Title { get; set; }
+    }
+}
