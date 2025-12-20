5af41bce70c6fc3c7bd8aecbe11506748f1f4760
suchithms19 <mssuchith2@gmail.com>
2025-12-21 01:50:21 +0530

    feat: add implementation and interface for images

---
+++ codePulse.API/Repositories/Implementation/ImageRepository.cs
@@ -0,0 +1,45 @@
+﻿using codePulse.AI.Data;
+using codePulse.AI.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace codePulse.API.Repositories.Implementation
+{
+    public class ImageRepository : IImageRepository
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly ApplicationDbContext dbContext;
+
+        public ImageRepository(IWebHostEnvironment webHostEnvironment,
+            IHttpContextAccessor httpContextAccessor,
+            ApplicationDbContext dbContext)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.httpContextAccessor = httpContextAccessor;
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<BlogImage>> GetAll()
+        {
+            return await dbContext.BlogImages.ToListAsync();
+        }
+
+        public async Task<BlogImage> Upload(IFormFile file, BlogImage blogImage)
+        {
+            var localPath = Path.Combine(webHostEnvironment.ContentRootPath, "Images", $"{blogImage.FileName}{blogImage.FileExtension}");
+            using var stream = new FileStream(localPath, FileMode.Create);
+            await file.CopyToAsync(stream);
+
+            var httpRequest = httpContextAccessor.HttpContext.Request;
+            var urlPath = $"{httpRequest.Scheme}://{httpRequest.Host}{httpRequest.PathBase}/Images/{blogImage.FileName}{blogImage.FileExtension}";
+
+            blogImage.Url = urlPath;
+
+            await dbContext.BlogImages.AddAsync(blogImage);
+            await dbContext.SaveChangesAsync();
+
+            return blogImage;
+        }
+    }
+}
