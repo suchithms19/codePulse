5c110c9ae7b2b1f4323419247ffe9d9fd70ba78b
suchithms19 <mssuchith2@gmail.com>
2025-12-20 15:22:49 +0530

    Implement many-to-many relation between BlogPosts and Categories

---
+++ codePulse.API/Migrations/20251220093610_Relate both tables.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace codePulse.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Relatebothtables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BlogPosts_BlogPosts_BlogPostId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_CategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_CategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BlogPosts_BlogPostId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "BlogPostId",
+                table: "BlogPosts");
+
+            migrationBuilder.CreateTable(
+                name: "BlogPostCategory",
+                columns: table => new
+                {
+                    BlogPostsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoriesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogPostCategory", x => new { x.BlogPostsId, x.CategoriesId });
+                    table.ForeignKey(
+                        name: "FK_BlogPostCategory_BlogPosts_BlogPostsId",
+                        column: x => x.BlogPostsId,
+                        principalTable: "BlogPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BlogPostCategory_Categories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogPostCategory_CategoriesId",
+                table: "BlogPostCategory",
+                column: "CategoriesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogPostCategory");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CategoryId",
+                table: "Categories",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "BlogPostId",
+                table: "BlogPosts",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CategoryId",
+                table: "Categories",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogPosts_BlogPostId",
+                table: "BlogPosts",
+                column: "BlogPostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BlogPosts_BlogPosts_BlogPostId",
+                table: "BlogPosts",
+                column: "BlogPostId",
+                principalTable: "BlogPosts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_CategoryId",
+                table: "Categories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+        }
+    }
+}
